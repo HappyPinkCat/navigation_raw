3cd699fbf8338f579d62fabad982c01eeb25de86
lpgong <lpgong@trip.com>
2024-01-09 19:02:22 +0800

    feat: 头像与logo

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import './Homepage.scss'
 import data from '../mock/list'
+import logo from '../static/logo.png';
+import avatar from '../static/avatar.png';
 export default function Homepage() {
   return (
     <div className='box_container'>
         <header>
-            <img src="" alt="" />
+            <div className='top_img_box'>
+              <img src={logo} alt="logo" className='logo'/>
+              <img src={avatar} alt="" className='avatar'/>
+            </div>
             <h1>Homepage</h1>
             <h3>some description:   Lorem  adipisicing. Tempore nesciunt dolor ut in! Lorem, ipsum dolor.    </h3>
             <input type="text" name="" id="" className='input_box'/>
